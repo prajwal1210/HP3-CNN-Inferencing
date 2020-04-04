0b345b6cdf3e8fe55cb9364089f744977c67149d
Tanay Bhartia <tanaybhartia@gmail.com>
2020-04-04 19:53:25 +0530

    updated func parameters in translator.h

--- proto/translator.h
+++ proto/translator.h
@@ -19,10 +19,10 @@ TODO: Add wrapper class here which calls the private classes.
 */
 
 private:
-    Conv2D translateConv2D_layer(DeepNet::Layer &layer, cudnnHandle_t cudnn);
-    Pool translatePool_layer(DeepNet::Layer &layer, cudnnHandle_t cudnn);
-    Activation translateActivation_layer(DeepNet::Layer& layer, cudnnHandle_t cudnn);
-    Linear translateLinear_layer(DeepNet::Layer& layer, cudnnHandle_t cudnn);
+    Conv2D translateConv2D_layer(DeepNet::Layer& layer, cudnnHandle_t cudnn,int h,int w, int batchsize);
+    Pool translatePool_layer(DeepNet::Layer& layer, cudnnHandle_t cudnn,int batchsize,int in_channels,int input_height,int input_weight);
+    Activation translateActivation_layer(DeepNet::Layer& layer, cudnnHandle_t cudnn, int batchsize,int in_channels,int input_height,int input_weight);
+    Linear translateLinear_layer(DeepNet::Layer& layer, cudnnHandle_t cudnn, int batchsize);
 }
 
 #endif
