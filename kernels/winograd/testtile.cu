3448ec5883f90997ae6017b80665f771906f853b
Vishal Gupta <Vishal0703@github.com>
2020-04-08 22:53:00 +0530

    pipeline completed

--- kernels/winograd/testtile.cu
+++ kernels/winograd/testtile.cu
@@ -7,7 +7,69 @@
 
 using namespace std;
 
-__global__ void tile(float *devin, float *devout, float *devsum, int h, int w)
+__global__ precompute(int out_channels, int input_channels, float* kernel_weights, float *U)
+{
+    int x = threadIdx.x;
+    int y = blockDim.x;;
+    int bid = blockIdx.x;
+    int offset = bid*y + x;
+    int m = 2, n = 3;
+    
+    float g[4][3] = {
+        {1, 0, 0},
+        {0.5, 0.5, 0.5},
+        {0.5, -0.5, 0.5},
+        {0, 0, 1}
+    };
+    
+    float g_t[3][4] ={
+        {1, 0.5, 0.5, 0},
+        {0, 0.5, -0.5, 0},
+        {0, 0.5, 0.5, 1}
+    };
+
+    float *temp = (float *)malloc(out_channels*input_channels*3*4*sizeof(float));
+    for(int i = 0; i <3; ++i)
+    {
+        for(int j = 0; j <4; ++j)
+        {
+            temp[offset*3*4+i*4+j] = 0;
+            for(int k = 0; k <3; ++k)
+            {
+                temp[offset*3*4+i*4+j] += kernel_weights[offset*3*3+i*3+k] * g_t[k][j];
+            }
+        }
+    }
+
+    for(int i = 0; i <4; ++i)
+    {
+        for(int j = 0; j <4; ++j)
+        {
+            U[offset*4*4+i*4+j] = 0;
+            for(int k = 0; k <3; ++k)
+            {
+                U[offset*4*4+i*4+j] += g[i][k] * temp[offset*3*4+k*4+j];
+            }
+        }
+    }
+}
+
+__global__ void uv(int tch, int out_channels, float *fin, float *U, float V[4][4])
+{
+    int x = threadIdx.x;
+    int offset = x*out_channels+tch;
+
+    for(int i = 0; i <4; ++i)
+    {
+        for(int j = 0; j <4; ++j)
+        {
+            fin[x*out_channels*4*4 + i*4 + j] = U[offset*4*4+i*4+j]*V[i][j];
+            
+        }
+    }
+}
+
+__global__ void tile(float *devin, float *devout, float *devsum, float *U, int h, int w, int och)
 {
     float thrtile[4][4];
     
@@ -36,16 +98,60 @@ __global__ void tile(float *devin, float *devout, float *devsum, int h, int w)
             thrtile[i][j] = devin[offset1 + th*w + tw];
         }
     }
+    float B[4][4] = {
+        {1,0,0,0},
+        {0,1,-1,1},
+        {-1,1,1,0},
+        {0,0,0,-1}
+    };
+
+    float B_t[4][4] = {
+        {1,0,-1,0},
+        {0,1,1,0},
+        {0,-1,1,0},
+        {0,1,0,-1}
+    };
+    //Calculation of V
+    for(int i = 0; i <4; ++i)
+    {
+        for(int j = 0; j <4; ++j)
+        {
+            temp[i][j] = 0;
+            for(int k = 0; k <4; ++k)
+            {
+                temp[i][j] += thrtile[i][k] * B[k][j];
+            }
+     
+        }
+    }
+
+    for(int i = 0; i <4; ++i)
+    {
+        for(int j = 0; j <4; ++j)
+        {
+            V[i][j] = 0;
+            for(int k = 0; k <4; ++k)
+            {
+                V[i][j] += B_t[i][k] * temp[k][j];
+            }
+        }
+    }
+/
+
+    float *fin = (float *)malloc(och*4*4);
+    uv<<<1,och>>>(tch, och, fin, U, V); 
 
     // copy thrtile to devout for testing
 
     int offset2 = (((tbs*p + tp)*q + tq)*ch + tch)*16;
-
-    for(int i = 0; i < 4; i++)
+    LOOP(och)
     {
-        for(int j = 0; j < 4; j++)
+        for(int i = 0; i < 4; i++)
         {
-            devout[offset2 + i*4 + j] = thrtile[i][j];
+            for(int j = 0; j < 4; j++)
+            {
+                devout[(((((tbs*och+toch)*p+tp)*q+tq)*ch+tch)*4 + i)*4 + j] = fin[(toch*4+i)*4+j];
+            }
         }
     }
 
@@ -59,11 +165,14 @@ __global__ void tile(float *devin, float *devout, float *devsum, int h, int w)
     {
         if(tch % (2*s) == 0 && tch+s < ch)
         {
-            for(int i = 0; i < 4; i++)
+            LOOP(och)
             {
-                for(int j = 0; j < 4; j++)
+                for(int i = 0; i < 4; i++)
                 {
-                    devout[offset3 + tch*16 + i*4 + j] += devout[offset3 + (tch+s)*16 + i*4 + j];
+                    for(int j = 0; j < 4; j++)
+                    {
+                        devout[(((((tbs*och+toch)*p+tp)*q+tq)*ch+tch)*4 + i)*4 + j] += devout[(((((tbs*och+toch)*p+tp)*q+tq)*ch+(tch+s))*4 + i)*4 + j];
+                    }
                 }
             }
         }
@@ -73,11 +182,14 @@ __global__ void tile(float *devin, float *devout, float *devsum, int h, int w)
     if(tch/*%ch*/ == 0) // can do with tch == 0
     {
         int offset = ((tbs*p + tp)*q + tq)*16;
-        for(int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 4; j++)
+        LOOP(och)
+        {    
+            for(int i = 0; i < 4; i++)
             {
-                devsum[offset + i*4 + j] = devout[offset3 + /*tch*16*/ +i*4 + j];
+                for(int j = 0; j < 4; j++)
+                {
+                    devsum[((((tbs*och+toch)*p+tp)*q+tq)*4 + i)*4 + j] = devout[(((((tbs*och+toch)*p+tp)*q+tq)*ch)*4 + i)*4 + j];
+                }
             }
         }
     }
@@ -93,7 +205,7 @@ void gpu_error(cudaError_t const &code)
     }
 }
 
-void tilehost(int och, int ch, int bs, int h, int w, float *&in, int &p, int &q, int &outsize, float *&out, int &sumsize, float *&sum)
+void tilehost(int och, int ch, int bs, int h, int w, float *&in, int &p, int &q, int &outsize, float *&out, int &sumsize, float *&sum, float *kernel_weights)
 {
     // int p, q;
     p = max((h-2)/2, 0);
@@ -102,8 +214,8 @@ void tilehost(int och, int ch, int bs, int h, int w, float *&in, int &p, int &q,
     float *devin, *devout, *devsum;
     devin = devout = devsum = nullptr;
     int insize = bs * ch * h * w * sizeof(float);
-    outsize = bs * p * q * ch * 4 * 4 * sizeof(float);
-    sumsize = bs * p * q * 4 * 4 * sizeof(float);
+    outsize = bs * och * p * q * ch * 4 * 4 * sizeof(float);
+    sumsize = bs * och * p * q * 4 * 4 * sizeof(float);
 
     gpu_error(cudaMalloc((void **) & devin, insize));
     gpu_error(cudaMalloc((void **) & devout, outsize));
@@ -113,16 +225,18 @@ void tilehost(int och, int ch, int bs, int h, int w, float *&in, int &p, int &q,
 
     // call the kernel function for tiling
     
+    float *U = (float *)malloc(och*ch*4*4*sizeof(float));
+    precompute<<<och, ch>>>(och, ch, kernel_weights, U);
+
     dim3 grid(bs, p, q);  // 3-D
     dim3 block(ch, 1, 1); // 1-D
-
-    tile<<<grid, block>>>(devin, devout, devsum, h, w);
+    tile<<<grid, block>>>(devin, devout, devsum, U, h, w, och);
 
     // copy from device to host to out.
 
     delete in;
-    out = new float[outsize];
-    sum = new float[sumsize];
+    out = new float[outsize/sizeof(float)];
+    sum = new float[sumsize/sizeof(float)];
 
     gpu_error(cudaMemcpy(out, devout, outsize, cudaMemcpyDeviceToHost));
     gpu_error(cudaMemcpy(sum, devsum, sumsize, cudaMemcpyDeviceToHost));
@@ -148,7 +262,7 @@ int main(void)
     int insize = bs * ch * h * w * sizeof(float);
     int outsize, sumsize;
  
-    float *in = new float[insize];
+    float *in = new float[insize/sizeof(float)];
     float *t = in;
     float *out, *sum;
  
@@ -188,7 +302,7 @@ int main(void)
 
     cout<<"\nTiling and Summing\n";
 
-    tilehost(1, ch, bs, h, w, in, p, q, outsize, out, sumsize, sum);
+    tilehost(1, ch, bs, h, w, in, p, q, outsize, out, sumsize, sum, kernel_weights);
     
     cout<<"\nTiling finished\n\n";
 
