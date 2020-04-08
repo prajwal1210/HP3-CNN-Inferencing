2996f1d16a8fecaa00765c2f354d97b4bab2d3b1
prajwal1210 <prajwal1210@gmail.com>
2020-04-08 08:55:29 +0530

    Fixed a small bug in direct_conv.cu; Basic Test passed; Still not working for the whole network

--- kernels/Direct/direct_conv.h
+++ kernels/Direct/direct_conv.h
@@ -1,4 +1,4 @@
-#ifndef DIRECT_CONV_H		/*include guard*/
+#ifndef DIRECT_CONV_H
 #define DIRECT_CONV_H
 
 #include <iostream>
@@ -8,10 +8,10 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-/*declaring forward pass function and class*/
 
 namespace Direct {
-	float* passforward(int out_channels, int input_channels, int kernel_height, int kernel_width, int padding, int stride, float* kernel_weights,int batchsize_of_data, int input_height, int input_width, float* input);
+	float* passforward(int out_channels, int input_channels, int kernel_height, int kernel_width, int padding, int stride, 
+					          float* kernel_weights,int batchsize_of_data, int input_height, int input_width, float* input);
 }
 
 #endif
