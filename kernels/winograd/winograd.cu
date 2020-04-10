8ac2317f94e9c189653dabda744b01f3ea2339af
Vishal Gupta <Vishal0703@github.com>
2020-04-11 02:51:08 +0530

    Broken into header and includer files

--- kernels/winograd/winograd.cu
+++ kernels/winograd/winograd.cu
@@ -1,7 +1,4 @@
-//%%cuda --name helloCUDA.cu
-#include <iostream>
-#include <random>
-#include <algorithm>
+#include "wingheader.h"
 
 #define LOOP(x) for(int t##x = 0; t##x < x; t##x++)
 #define cudaSafeCall(call)  \
@@ -15,12 +12,11 @@
             }\
         } while(0)
 
-using namespace std;
 
 void gpu_error(cudaError_t const &code) {
     if(code != cudaSuccess)
     {
-        cerr << "GPUError: Code " << code << " : " << cudaGetErrorString(code) << endl;
+        std::cerr << "GPUError: Code " << code << " : " << cudaGetErrorString(code) << std::endl;
         exit( EXIT_FAILURE );
     }
 }
@@ -237,7 +233,7 @@ __global__ void tile(int bs, int p, int q, int ch, float *devin, float *devout, 
       cudaDeviceSynchronize();
   }
 }
-float * forward(int och, int ch, int bs, int h, int w, int pad, float *&in, int &oph, int &opw, float *kwt)
+float * WING::forward(int och, int ch, int bs, int h, int w, int pad, float *&in, int &oph, int &opw, float *kwt)
 {
     float *devin, *devinnopad, *cutY;
     int insize = bs * ch * h * w * sizeof(float);
@@ -323,93 +319,4 @@ float * forward(int och, int ch, int bs, int h, int w, int pad, float *&in, int 
     gpu_error(cudaFree(devcutY));
 
     return cutY;
-}
-
-int main(void) 
-{
-    auto engine = default_random_engine(7);
-    auto rng = uniform_real_distribution<float>();
-    int bs, ch, h, w, och, pad; 
-    //bs - batch size, ch - input channel, h - input height, w - input weight , pad - padding required
-    
-    bs = 2;
-    ch = 2;
-    h = 5;
-    w = 5;
-    och = 1;
-    pad = 0;
-    int insize = bs * ch * h * w * sizeof(float);
-    float *in = new float[insize/sizeof(float)];
-    float *t = in;
-    float  *cutY; //final convolved output
-    float *kernel_weights = new float[och*3*3*ch];
-    int tsize = och*ch*3*3;
-    float *tkw = kernel_weights;
-    //put kernel weights
-    LOOP(tsize)
-    {
-        tkw[ttsize] = 0;
-    }
-    tkw[0] = tkw[8] = 1;
-    tkw[9] = tkw[17] = 1;
-    //put input
-    LOOP(bs)
-    {
-        LOOP(ch)
-        {
-            LOOP(h)
-            {
-                LOOP(w)
-                {
-                    *(t++) = rng(engine);
-                }
-            }
-        }
-    }
-    // LOOP(bs)
-    // {
-    //     cout<<"{ ";
-    //     LOOP(ch)
-    //     {
-    //         cout<<"{ ";
-    //         LOOP(h)
-    //         {
-    //             cout<<"{ ";
-    //             LOOP(w)
-    //             {
-    //                 cout<<in[((tbs*ch+tch)*h+th)*w+tw]<<" ";
-    //             }
-    //             cout<<"}\n";
-    //         }
-    //         cout<<"}\n";
-    //     }
-    //     cout<<"}\n";
-    // }
-    cout<<"\nConvolving\n";
-    
-    int oph, opw; //output height, output weight
-    cutY = forward(och, ch, bs, h, w, pad, in, oph, opw, kernel_weights);
-
-    cout<<"\nConvolution finished\n\n";
-      
-    LOOP(bs)
-    {
-        cout<<"{ ";
-        LOOP(och)
-        {
-            cout<<"{ ";
-            LOOP(oph)
-            {
-                LOOP(opw)
-                {
-                    cout<<cutY[((tbs*och+toch)*oph+toph)*opw+topw]<<",";
-                }
-                cout<<";\n";
-            }
-            cout<<"}\n";
-        }
-        cout<<"}\n";
-    }
-    cout<<"}\n";
-    return 0;
 }
---
+++ kernels/winograd/winograd_test.cu
@@ -0,0 +1,94 @@
+#include "winograd.cu"
+
+#include<random>
+#define LOOP(x) for(int t##x = 0; t##x < x; t##x++)
+using namespace std;
+
+int main(void) 
+{
+    auto engine = default_random_engine(7);
+    auto rng = uniform_real_distribution<float>();
+    int bs, ch, h, w, och, pad; 
+    //bs - batch size, ch - input channel, h - input height, w - input weight , pad - padding required
+    
+    bs = 2;
+    ch = 2;
+    h = 5;
+    w = 5;
+    och = 1;
+    pad = 0;
+    int insize = bs * ch * h * w * sizeof(float);
+    float *in = new float[insize/sizeof(float)];
+    float *t = in;
+    float  *cutY; //final convolved output
+    float *kernel_weights = new float[och*3*3*ch];
+    int tsize = och*ch*3*3;
+    float *tkw = kernel_weights;
+    //put kernel weights
+    LOOP(tsize)
+    {
+        tkw[ttsize] = 0;
+    }
+    tkw[0] = tkw[8] = 1;
+    tkw[9] = tkw[17] = 1;
+    //put input
+    LOOP(bs)
+    {
+        LOOP(ch)
+        {
+            LOOP(h)
+            {
+                LOOP(w)
+                {
+                    *(t++) = rng(engine);
+                }
+            }
+        }
+    }
+    // LOOP(bs)
+    // {
+    //     cout<<"{ ";
+    //     LOOP(ch)
+    //     {
+    //         cout<<"{ ";
+    //         LOOP(h)
+    //         {
+    //             cout<<"{ ";
+    //             LOOP(w)
+    //             {
+    //                 cout<<in[((tbs*ch+tch)*h+th)*w+tw]<<" ";
+    //             }
+    //             cout<<"}\n";
+    //         }
+    //         cout<<"}\n";
+    //     }
+    //     cout<<"}\n";
+    // }
+    cout<<"\nConvolving\n";
+    
+    int oph, opw; //output height, output weight
+    cutY = WING::forward(och, ch, bs, h, w, pad, in, oph, opw, kernel_weights);
+
+    cout<<"\nConvolution finished\n\n";
+      
+    LOOP(bs)
+    {
+        cout<<"{ ";
+        LOOP(och)
+        {
+            cout<<"{ ";
+            LOOP(oph)
+            {
+                LOOP(opw)
+                {
+                    cout<<cutY[((tbs*och+toch)*oph+toph)*opw+topw]<<",";
+                }
+                cout<<";\n";
+            }
+            cout<<"}\n";
+        }
+        cout<<"}\n";
+    }
+    cout<<"}\n";
+    return 0;
+}
