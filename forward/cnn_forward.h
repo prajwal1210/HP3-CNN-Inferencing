cf0a943bfc23c98e5d9c0fa65f15ec884b913be1
prajwal1210 <prajwal1210@gmail.com>
2020-04-20 03:45:13 +0530

    [Documentation] Fixed Comments

--- forward/cnn_forward.h
+++ forward/cnn_forward.h
@@ -29,8 +29,9 @@ namespace CNN {
   void loadCNNModelFromFile(const char* model_file, DeepNet::Network& net);
   
   /* Iterates over the CNN Network and computes the forward pass on the provided data
-   * Input  : DeepNet::Network object, references to dimensions of the input, pointer to the input array, reference to the status flag
-   * Output : Pointer to the final layer output (side effect - sets the dimensions accordingly)
+   * Input  : DeepNet::Network object, references to dimensions of the input, pointer to the input array, Algorithm to use for Convolution,
+   *          Reference to a vector to store the elapsed time details, Reference to the status flag
+   * Output : Pointer to the final layer output (side effect - sets the dimensions accordingly, add the elapsed time details for convolution in the vector)
    */
   float* forwardPass(DeepNet::Network net, int& batchsize, int& input_h, int& input_w, int& input_c, float* input, 
                     customAlgorithmType algo, std::vector<profilingElapsedTime>& time_elapsed, bool& succes);
